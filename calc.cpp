764fce1c4a391994e0159014213c5d4a3f158782
Gregori Kempf <204034@upf.br>
2025-02-18 20:21:16 -0300

    simmmmmmm

---
+++ calc.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int option;
+    int a, b;
+    cout << "Escolha uma opção\n";
+
+    cout << "Mais, Digite 1\n";
+    cout << "Menos, Digite 2\n";
+    cout << "Vezes, Digite 3\n";
+    cout << "Dividir, Digite 4\n";
+    cin >> option;
+
+    switch(option){
+        case 1:
+                cout << "one";
+                break;
+        case 2:
+                cout << "one";
+                break;
+        case 3:
+                cout << "one";
+                break;
+        case 4:
+                cout << "one";
+                break;
+    }
+
+}
